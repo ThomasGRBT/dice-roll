d7c345d90b272eb81a282fea3fcc19d5a880386e
ThomasGRBT <102649473+ThomasGRBT@users.noreply.github.com>
2023-09-19 17:38:04 +0200

    Add files via upload

---
+++ source/wordpress-version/view.php
@@ -0,0 +1,6 @@
+<?php 
+
+    echo '<h1 class="wp-heading-inline">Web En Chartreuse - Dice Roll Plugin</h1>';
+    
+    echo '<hr class="wp-header-end">';
+    echo '<p>Please add this shortcode to display 2D6 Roll Dice : [wec-dice-roll] </p>';
